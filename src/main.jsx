8db7d9f5631ac8dd9f9838ee82d99168e976911c
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-05-21 21:42:14 -0500

    Agregando error-page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './routes/root'
+import Contact from './routes/contact'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from "./error-page";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>
-  }
+    element: <Root/>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>,
+      }
+    ]
+  },
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
